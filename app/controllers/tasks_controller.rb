c986b06439b16d89d4c3f9e34a809170280a9eb9
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-09-12 10:54:54 +0200

    add task create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,19 @@ class TasksController < ApplicationController
     @tasks = Task.all
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: 'Task successfully created'
+    else
+      render :new, status: :unproccessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
+  end
 end
